f1843ed666fb738e94c3d58eab181f54554a6391
thepratholic <chelaramanipratham@gmail.com>
2026-04-20 06:48:51 +0530

    20/4/26

---
+++ LeetCode/Two Furthest Houses With Different Colors.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def maxDistance(self, colors: List[int]) -> int:
+        n = len(colors)
+        ans = 0
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if colors[i] != colors[j]:
+                    ans = max(ans, abs(i - j))
+
+        return ans
