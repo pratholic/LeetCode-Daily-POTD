804b92cc6fa92f7d35b9fe02ef71eb7b5300e8be
thepratholic <chelaramanipratham@gmail.com>
2026-05-06 07:50:13 +0530

    6/5/26

--- LeetCode/Rotating the box.py
+++ LeetCode/Rotating the box.py
@@ -2,22 +2,29 @@ from typing import List
 
 
 class Solution:
-    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
-        rows, cols = len(box), len(box[0])
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        m, n = len(boxGrid), len(boxGrid[0])
 
-        for r in reversed(range(rows)):
-            i = cols - 1
-            for c in reversed(range(cols)):
-                if box[r][c] == "#":
-                    box[r][c], box[r][i] = box[r][i], box[r][c]
-                    i -= 1
-                elif box[r][c] == "*":
-                    i = c - 1
+        ans = [[''] * m for _ in range(n)]
 
-        res = []
-        for c in range(cols):
-            col = []
-            for r in reversed(range(rows)):
-                col.append(box[r][c])
-            res.append(col)
-        return res
+        for i in range(m):
+            for j in range(n):
+                ans[j][m - i - 1] = boxGrid[i][j]
+
+        rows = n
+        cols = m
+
+        for col in range(m):
+            write = rows - 1
+
+            for row in range(rows - 1, -1, -1):
+                if ans[row][col] == '*':
+                    write = row - 1 # stones iske upar hi aayenge
+
+                elif ans[row][col] == '#':
+                    ans[row][col] = '.'
+                    ans[write][col] = '#'
+
+                    write -= 1
+
+        return ans
